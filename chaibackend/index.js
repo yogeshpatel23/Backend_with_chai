39cf56780b4c13621b1f738a9f4df81542de2d12
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-09 11:29:18 +0530

    first test

---
+++ chaibackend/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Hello Twitter");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login at chai and code</h1>");
+});
+
+app.get("/chai", (req, res) => {
+  res.send("<h2>this is h2 tag </h2>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Example app listening on port http://localhost:${process.env.PORT}`
+  );
+});
