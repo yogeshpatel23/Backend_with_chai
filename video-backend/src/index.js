d5a09bdaaad5d0afb077d7fd149707cb40760bb2
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-10 15:38:20 +0530

    Util setup

--- video-backend/src/index.js
+++ video-backend/src/index.js
@@ -1,9 +1,16 @@
 // require("dotenv").config();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MongoDb connection failed !!", err));
