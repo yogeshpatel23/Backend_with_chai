2e54b769e3565cb3147465307811e028cc8a796d
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-10 19:21:55 +0530

    Near Setup Compleate

--- video-backend/src/app.js
+++ video-backend/src/app.js
@@ -17,4 +17,10 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes decleration
+app.use("/api/v1/users", userRouter);
+
 export { app };
